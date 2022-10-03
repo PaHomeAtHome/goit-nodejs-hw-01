b1ebf54130c502d627cd40d9594d7f0c9e0c2c61
Andrey Pakhomov <pahomeathome@gmail.com>
2022-10-03 18:07:02 +0300

    yargs added

--- contacts.js
+++ contacts.js
@@ -1,19 +1,47 @@
 const fs = require("fs").promises;
 const path = require("path");
 const contactsPath = path.resolve("db/contacts.json");
+const testPath = path.resolve("contactsTest/contacts.json");
 
 function listContacts() {
-  fs.readFile(contactsPath)
+  fs.readFile(testPath)
     .then((data) => console.log(data.toString()))
     .catch((err) => console.log(err.message));
 }
 
 function getContactById(contactId) {
-  // ...твій код
+  fs.readFile(testPath)
+    .then((data) =>
+      JSON.parse(data).find(
+        (contact) => contact.id.toString() === contactId.toString()
+      )
+    )
+    .then((result) =>
+      result ? console.log(result) : console.log("Contact not found")
+    )
+    .catch((err) => console.log(err.message));
 }
 
 function removeContact(contactId) {
-  // ...твій код
+  fs.readFile(testPath)
+    .then((data) =>
+      JSON.parse(data).find(
+        (contact) => contact.id.toString() === contactId.toString()
+      )
+        ? JSON.parse(data).filter(
+            (contact) => contact.id.toString() !== contactId.toString()
+          )
+        : (result = null)
+    )
+    .then((result) =>
+      result !== null
+        ? fs
+            .writeFile(testPath, JSON.stringify(result))
+            .then(() => console.log(`Contact with ID ${contactId} removed`))
+            .catch((err) => console.log(err.message))
+        : console.log("Contact not found")
+    )
+    .catch((err) => console.log(err.message));
 }
 
 function addContact(name, email, phone) {
