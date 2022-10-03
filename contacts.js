5e4458926fb894d3e1e584f7e471d9852b8fea9a
Andrey Pakhomov <pahomeathome@gmail.com>
2022-10-03 15:30:50 +0300

    main files added

---
+++ contacts.js
@@ -0,0 +1,23 @@
+const fs = require("fs").promises;
+const path = require("path");
+const contactsPath = path.resolve("db/contacts.json");
+
+function listContacts() {
+  fs.readFile(contactsPath)
+    .then((data) => console.log(data.toString()))
+    .catch((err) => console.log(err.message));
+}
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
